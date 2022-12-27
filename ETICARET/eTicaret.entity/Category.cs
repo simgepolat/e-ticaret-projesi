7638eddc05627581701444f4b21a2e509867a9a8
Simgepolat <simgepolat15@gmail.com>
2022-12-27 15:16:27 +0300

    added entity

---
+++ ETICARET/eTicaret.entity/Category.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicaret.entity
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public List<ProductCategory> ProductCategory { get; set; }
+    }
+}
