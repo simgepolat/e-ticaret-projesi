943f5b7e8cda559c2b76aebcf04030cc93fbc577
Simgepolat <simgepolat15@gmail.com>
2022-12-27 12:53:52 +0300

    edit product update form
    error resolved

--- ETICARET/eTicaret/Models/Product.cs
+++ ETICARET/eTicaret/Models/Product.cs
@@ -8,10 +8,10 @@ namespace eTicaret.Models
     public class Product
     {
         public int ProductId { get; set; }
-        public string ImageUrl { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
+         public string ImageUrl { get; set; }
         public bool IsApproved { get; set; }
         public int CategoryId { get; set; }
     }
