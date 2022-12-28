16cf0fad18035050e358a276ce747315c2328d46
Simgepolat <simgepolat15@gmail.com>
2022-12-29 02:57:25 +0300

    admin screen product registration

--- ETICARET/eTicaret/Controllers/AdminController.cs
+++ ETICARET/eTicaret/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using eTicaret.business.Abstract;
+using eTicaret.entity;
 using eTicaret.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -22,6 +23,31 @@ namespace eTicaret.Controllers
             {
                 Products = _productService.GetAll()
             });
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel model)
+        {
+            var entity = new Product()
+            {
+                Name = model.Name,
+                Url = model.Url,
+                Price = model.Price,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl
+            };
+            
+            _productService.Create(entity);
+
+            return RedirectToAction("ProductList");
         }
     }
 }
--- ETICARET/eTicaret/Models/ProductModel.cs
+++ ETICARET/eTicaret/Models/ProductModel.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace eTicaret.Models
 {
     public class ProductModel
     {
         public int ProductId { get; set; }  
+        [Display(Name="Name",Prompt="Ürün adı giriniz.")]
         public string Name { get; set; }       
+        public string Url { get; set; }  
+        [Display(Name="Fiyat")]     
         public double? Price { get; set; } 
-        public string Description { get; set; }         
+        [Display(Name="Açıklama")] 
+        public string Description { get; set; }  
+        [Display(Name="resim")]        
         public string ImageUrl { get; set; }
         public bool IsApproved { get; set; }
+        public bool IsHome { get; set; }
     }
 }
